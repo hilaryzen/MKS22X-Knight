c073d7f69c5409123d53bca202c89abee99a38f1
Hilary Zen <hzen00@stuy.edu>
2019-02-13 10:51:02 -0500

    Added solve and solveH methods

--- KnightBoard.java
+++ KnightBoard.java
@@ -8,6 +8,8 @@ public class KnightBoard {
     board = new int[startingRows][startingCols];
   }
 
+  //Blank boards display underscores
+  //Otherwise displays numbers showing knight's path
   public String toString() {
     String ans = "";
     for (int i = 0; i < board.length; i++) {
@@ -21,5 +23,17 @@ public class KnightBoard {
       ans += '\n';
     }
     return ans;
+  }
+
+  //Modifies the board by labeling the moves from 1 to the area of board in knight move steps
+  public boolean solve(int startingRow, int startingCol) {
+    return true;
+  }
+
+  private boolean solveH(int row, int col, int level) {
+    if (level > board.length * board[0].length) {
+      return true;
+    }
+    return false;
   }
 }
