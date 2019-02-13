04a32ac415567a0397073b223b1e9749e7bfa5a0
Hilary Zen <hzen00@stuy.edu>
2019-02-13 10:32:06 -0500

    Wrote basic toString

--- KnightBoard.java
+++ KnightBoard.java
@@ -4,4 +4,16 @@ public class KnightBoard {
   public KnightBoard(int startingRows, int startingCols) {
     board = new int[startingRows][startingCols];
   }
+
+  public String toString() {
+    String ans = "";
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        if (board[i][j] == 0) {
+          ans += "_ ";
+        }
+      }
+      ans += '\n';
+    }
+  }
 }
