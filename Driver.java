15d1e3ec2dd7d8548926e62017bec946b83b99df
Hilary Zen <hzen00@stuy.edu>
2019-02-13 10:38:24 -0500

    Added exception in toString

--- Driver.java
+++ Driver.java
@@ -2,5 +2,8 @@ public class Driver {
   public static void main(String[] args) {
     KnightBoard board = new KnightBoard(3,3);
     System.out.println(board);
+
+    //Should throw exception
+    //KnightBoard board1 = new KnightBoard(0,3);
   }
 }
--- KnightBoard.java
+++ KnightBoard.java
@@ -2,6 +2,9 @@ public class KnightBoard {
   private int[][] board;
 
   public KnightBoard(int startingRows, int startingCols) {
+    if (startingRows <= 0 || startingCols <= 0) {
+      throw new IllegalArgumentException();
+    }
     board = new int[startingRows][startingCols];
   }
 
