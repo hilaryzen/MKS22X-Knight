e18cd4ce408fb83b30a8ddf070b057f84b303c3d
Hilary Zen <hzen00@stuy.edu>
2019-02-14 10:44:31 -0500

    Added for loop

--- KnightBoard.java
+++ KnightBoard.java
@@ -29,14 +29,14 @@ public class KnightBoard {
   public boolean solve(int startingRow, int startingCol) {
     int[] movesR = {-2, -1, 1, 2, 2, 1, -1, -2};
     int[] movesC = {1, 2, 2, 1, -1, -2, -2, -1};
-    return solveH(0,0,1);
+    return solveH(0,0,1, movesR, movesC);
   }
 
-  private boolean solveH(int row, int col, int level) {
-    if (level > board.length * board[0].length) {
+  private boolean solveH(int row, int col, int level, int[] movesR, int[] movesC) {
+    if (level > board.length * board[0].length) { //Checks if all squares have been filled
       return true;
     } else {
-      if (board[row + 2][col + 1] == 0) { //Checks if knight can move to a square
+      for (int i = 0; i < movesR.length; i++) { //Loops through all possible moves
         if (solveH(row + 2, col + 1, level + 1)) {
           board[row + 2][col + 1] = level;
           return true;
