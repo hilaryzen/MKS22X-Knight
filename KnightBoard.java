73b35cbae60681621f34c5f1af02a6270c0baaf4
Hilary Zen <hzen00@stuy.edu>
2019-02-22 01:44:22 -0500

    Fixed toString

--- KnightBoard.java
+++ KnightBoard.java
@@ -15,9 +15,17 @@ public class KnightBoard {
     for (int i = 0; i < board.length; i++) {
       for (int j = 0; j < board[i].length; j++) {
         if (board[i][j] == 0) {
-          ans += "_ ";
+          if (board.length * board[0].length >= 10) {
+            ans += " _ ";
+          } else {
+            ans += "_ ";
+          }
         } else {
-          ans = ans + board[i][j] + " ";
+          if (board.length * board[0].length >= 10 && board[i][j] / 10 == 0) {
+            ans = ans + " " + board[i][j] + " ";
+          } else {
+            ans = ans + board[i][j] + " ";
+          }
         }
       }
       ans += '\n';
